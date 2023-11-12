e3c4e5b7976f1c3a040141089002f6d6b52110c1
KANG DONGHA <knamuuu@naver.com>
2023-11-13 01:02:13 +0900

    Feat: password 정규식 추가

--- src/components/organisms/Sign/SignupContainer/index.tsx
+++ src/components/organisms/Sign/SignupContainer/index.tsx
@@ -7,6 +7,8 @@ import request from 'utils/axios';
 
 import styles from './styles.module.scss';
 
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])(?=.{7,})/;
+
 export default function SignupContainer() {
     const [signupForm, setSignupForm] = useState<ISignupForm>({
         account: '',
@@ -16,15 +18,25 @@ export default function SignupContainer() {
     });
 
     const [confirmPassword, setConfirmPassword] = useState<string>('');
-    const [passwordMismatch, setPasswordMismatch] = useState<boolean>(true);
+    const [isPasswordValid, setIsPasswordValid] = useState(false);
+    const [isPasswordMatch, setIsPasswordMatch] = useState(false);
 
     const onChangeLoginForm = (key: keyof ISignupForm, value: string) => {
         setSignupForm((prev) => ({ ...prev, [key]: value }));
+
+        if (key === 'password') {
+            setIsPasswordValid(passwordRegex.test(value));
+        }
+    };
+
+    const onConfirmPasswordChange = (value: string) => {
+        setConfirmPassword(value);
+        setIsPasswordMatch(value === signupForm.password);
     };
 
     const onClickSignupButton = async () => {
-        if (passwordMismatch) {
-            alert('확인 비밀번호가 일치하지 않습니다.');
+        if (!isPasswordValid || !isPasswordMatch) {
+            alert('입력 정보를 다시 확인해주세요!');
             return;
         }
 
@@ -46,23 +58,27 @@ export default function SignupContainer() {
                     placeholder="ID"
                     onChange={(e) => onChangeLoginForm('account', e.target.value)}
                 />
-                <input
-                    type="password"
-                    value={signupForm.password}
-                    placeholder="Password"
-                    onChange={(e) => onChangeLoginForm('password', e.target.value)}
-                />
+                <div className={styles.inputContainer}>
+                    <input
+                        type="password"
+                        value={signupForm.password}
+                        placeholder="Password"
+                        onChange={(e) => onChangeLoginForm('password', e.target.value)}
+                    />
+                    {signupForm.password && !isPasswordValid && (
+                        <span className={styles.errMsg}>
+                            비밀번호는 대문자, 소문자, 특수기호를 포함하며 7자 이상이어야 합니다.
+                        </span>
+                    )}
+                </div>
                 <div className={styles.inputContainer}>
                     <input
                         type="password"
                         value={confirmPassword}
                         placeholder="Confirm Password"
-                        onChange={(e) => {
-                            setConfirmPassword(e.target.value);
-                            setPasswordMismatch(signupForm.password !== e.target.value);
-                        }}
+                        onChange={(e) => onConfirmPasswordChange(e.target.value)}
                     />
-                    {confirmPassword && passwordMismatch && (
+                    {confirmPassword && !isPasswordMatch && (
                         <span className={styles.errMsg}>확인 비밀번호가 일치하지 않습니다.</span>
                     )}
                 </div>
