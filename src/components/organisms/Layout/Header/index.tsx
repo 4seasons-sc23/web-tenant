ad6308699e169562f9908f57d17a02c65c1f18fc
KANG DONGHA <knamuuu@naver.com>
2023-11-13 01:21:22 +0900

    Feat: Header component에 logout 추가

--- src/components/organisms/Layout/Header/index.tsx
+++ src/components/organisms/Layout/Header/index.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
 export default function Header() {
     const navigate = useNavigate();
 
-    const userNickname = '1';
+    const name = localStorage.getItem('name');
 
     const onClickLogo = () => {
         navigate('/');
@@ -19,14 +19,28 @@ export default function Header() {
         navigate('/signup');
     };
 
+    const onClickLogoutButton = () => {
+        if (window.confirm('정말 로그아웃 하시겠습니까 ?')) {
+            localStorage.removeItem('id');
+            localStorage.removeItem('name');
+            window.location.href = '/';
+        }
+    };
+
     return (
         <div className={styles.header}>
             <div onClick={onClickLogo} className={styles.header_logo}>
                 <h2>IN-STREAM</h2>
             </div>
             <div>
-                {!userNickname ? (
-                    <div className={styles.header_info_user}>{userNickname}</div>
+                {name ? (
+                    <div className={styles.buttonArea}>
+                        <div>{name}</div>
+                        <div className={styles.divider} />
+                        <button onClick={onClickLogoutButton} className={styles.button}>
+                            Log out
+                        </button>
+                    </div>
                 ) : (
                     <div className={styles.buttonArea}>
                         <button onClick={onClickSigninButton} className={styles.button}>
