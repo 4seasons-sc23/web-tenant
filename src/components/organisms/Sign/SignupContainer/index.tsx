f4f239fbd6b65f2fc2d636a97e0a72e31e51fcbd
KANG DONGHA <knamuuu@naver.com>
2023-11-13 01:11:18 +0900

    Feat: phoneNumber 정규식 추가

--- src/components/organisms/Sign/SignupContainer/index.tsx
+++ src/components/organisms/Sign/SignupContainer/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { isAxiosError } from 'axios';
 
 import { ISignupForm } from 'types/account';
@@ -8,8 +9,11 @@ import request from 'utils/axios';
 import styles from './styles.module.scss';
 
 const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])(?=.{7,})/;
+const phoneNumberRegex = /^\d{3}-\d{4}-\d{4}$/;
 
 export default function SignupContainer() {
+    const navigate = useNavigate();
+
     const [signupForm, setSignupForm] = useState<ISignupForm>({
         account: '',
         password: '',
@@ -20,12 +24,15 @@ export default function SignupContainer() {
     const [confirmPassword, setConfirmPassword] = useState<string>('');
     const [isPasswordValid, setIsPasswordValid] = useState(false);
     const [isPasswordMatch, setIsPasswordMatch] = useState(false);
+    const [isPhoneNumberValid, setIsPhoneNumberValid] = useState(false);
 
     const onChangeLoginForm = (key: keyof ISignupForm, value: string) => {
         setSignupForm((prev) => ({ ...prev, [key]: value }));
 
         if (key === 'password') {
             setIsPasswordValid(passwordRegex.test(value));
+        } else if (key === 'phoneNumber') {
+            setIsPhoneNumberValid(phoneNumberRegex.test(value));
         }
     };
 
@@ -35,14 +42,15 @@ export default function SignupContainer() {
     };
 
     const onClickSignupButton = async () => {
-        if (!isPasswordValid || !isPasswordMatch) {
+        if (!isPasswordValid || !isPasswordMatch || !isPhoneNumberValid) {
             alert('입력 정보를 다시 확인해주세요!');
             return;
         }
 
         try {
-            const signup = await request('POST', '/v1/hosts/sign-up', signupForm);
-            console.log(signup);
+            await request('POST', '/v1/hosts/sign-up', signupForm);
+            alert('회원가입이 완료되었습니다 !');
+            navigate('/signin');
         } catch (e) {
             if (isAxiosError(e) && e.response) alert(e.response.data.message);
             else alert('알 수 없는 에러가 발생했습니다.');
@@ -87,11 +95,16 @@ export default function SignupContainer() {
                     placeholder="Name"
                     onChange={(e) => onChangeLoginForm('name', e.target.value)}
                 />
-                <input
-                    value={signupForm.phoneNumber}
-                    placeholder="PhoneNumber (ex. 010-0000-0000)"
-                    onChange={(e) => onChangeLoginForm('phoneNumber', e.target.value)}
-                />
+                <div className={styles.inputContainer}>
+                    <input
+                        value={signupForm.phoneNumber}
+                        placeholder="PhoneNumber (ex. 010-0000-0000)"
+                        onChange={(e) => onChangeLoginForm('phoneNumber', e.target.value)}
+                    />
+                    {signupForm.phoneNumber && !isPhoneNumberValid && (
+                        <span className={styles.errMsg}>올바른 전화번호를 입력해주세요.</span>
+                    )}
+                </div>
             </div>
             <div className={styles.buttonArea}>
                 <button className={styles.submitButton} onClick={onClickSignupButton}>
