1c1ee8ba33c639f899a9e8c561e9815c5b400bf7
KANG DONGHA <knamuuu@naver.com>
2023-12-01 15:19:58 +0900

    Feat: session 추가 버튼 생성

--- src/pages/Sessions/index.tsx
+++ src/pages/Sessions/index.tsx
@@ -18,8 +18,6 @@ interface ISession {
     deletedAt: string;
 }
 
-const id = window.localStorage.getItem('id');
-
 export default function Sessions() {
     const navigate = useNavigate();
 
@@ -55,57 +53,76 @@ export default function Sessions() {
         getSessionsList(firstView);
     }, [currentPage]);
 
+    const onClickAddSession = async () => {
+        try {
+            await request('PATCH', `/v1/applications/${applicationId}/sessions/start`, null, {
+                ApiKey,
+            });
+            getSessionsList(true);
+        } catch (e) {
+            if (isAxiosError(e)) alert(e.response?.data.message);
+        }
+    };
+
     return (
-        <div className={styles.container}>
-            {sessionsList.length !== 0 ? (
-                <>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>sessionId</th>
-                                <th></th>
-                                <th>createdAt</th>
-                                <th>deletedAt</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {sessionsList.map((session) => (
+        <>
+            <div className={styles.buttonArea}>
+                <button onClick={onClickAddSession}>
+                    <span>Add Session</span>
+                    <span>+</span>
+                </button>
+            </div>
+            <div className={styles.container}>
+                {sessionsList.length !== 0 ? (
+                    <>
+                        <table>
+                            <thead>
                                 <tr>
-                                    <td
-                                        onClick={() => {
-                                            navigate(
-                                                `/participant?ApiKey=${ApiKey}&sessionId=${session.id}`
-                                            );
-                                        }}
-                                        style={{ cursor: 'pointer' }}
-                                    >
-                                        {session.id}
-                                    </td>
-                                    <td style={{ width: '3%' }}>
-                                        <FaRegCopy
-                                            style={{ cursor: 'pointer' }}
-                                            onClick={async () => {
-                                                await navigator.clipboard.writeText(session.id);
-                                                alert('클립보드에 복사되었습니다!');
-                                            }}
-                                        />
-                                    </td>
-                                    <td>{dateForm(session.createdAt)}</td>
-                                    <td>{dateForm(session.deletedAt)}</td>
+                                    <th>sessionId</th>
+                                    <th></th>
+                                    <th>createdAt</th>
+                                    <th>deletedAt</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                    <PaginationComponent
-                        currentPage={currentPage}
-                        setPage={setCurrentPage}
-                        pageCount={pageCount}
-                        setFirstView={setFirstView}
-                    />
-                </>
-            ) : (
-                <div>생성된 세션이 없습니다.</div>
-            )}
-        </div>
+                            </thead>
+                            <tbody>
+                                {sessionsList.map((session) => (
+                                    <tr>
+                                        <td
+                                            onClick={() => {
+                                                navigate(
+                                                    `/participant?ApiKey=${ApiKey}&sessionId=${session.id}`
+                                                );
+                                            }}
+                                            style={{ cursor: 'pointer' }}
+                                        >
+                                            {session.id}
+                                        </td>
+                                        <td style={{ width: '3%' }}>
+                                            <FaRegCopy
+                                                style={{ cursor: 'pointer' }}
+                                                onClick={async () => {
+                                                    await navigator.clipboard.writeText(session.id);
+                                                    alert('클립보드에 복사되었습니다!');
+                                                }}
+                                            />
+                                        </td>
+                                        <td>{dateForm(session.createdAt)}</td>
+                                        <td>{dateForm(session.deletedAt)}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <PaginationComponent
+                            currentPage={currentPage}
+                            setPage={setCurrentPage}
+                            pageCount={pageCount}
+                            setFirstView={setFirstView}
+                        />
+                    </>
+                ) : (
+                    <div>생성된 세션이 없습니다.</div>
+                )}
+            </div>
+        </>
     );
 }
