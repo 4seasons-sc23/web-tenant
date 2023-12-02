3bafda1f1d5542de74cb5090f6192232e2c58f48
KANG DONGHA <knamuuu@naver.com>
2023-12-02 16:56:32 +0900

    Feat: error 수정하기 기능

--- src/pages/Question/Post/index.tsx
+++ src/pages/Question/Post/index.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { isAxiosError } from 'axios';
 
 import request from 'utils/axios';
 
 import styles from './styles.module.scss';
 
-const id = window.localStorage.getItem('id');
+const hostId = window.localStorage.getItem('id');
 
 export default function PostQuestion() {
     const navigate = useNavigate();
@@ -14,14 +14,45 @@ export default function PostQuestion() {
     const [title, setTitle] = useState<string>('');
     const [content, setContent] = useState<string>('');
 
+    const { id } = useParams();
+
+    useEffect(() => {
+        const getErrorData = async () => {
+            try {
+                const res = await request('GET', `/v1/errors/${id}`);
+
+                setTitle(res.question.title);
+                setContent(res.question.content);
+            } catch (e) {
+                if (isAxiosError(e)) alert(e.response?.data.message);
+            }
+        };
+
+        if (id !== '0') getErrorData();
+    }, []);
+
     const postError = async () => {
         if (!(title && content)) {
             alert('내용을 작성해주세요');
             return;
         }
         try {
-            await request('POST', '/v1/errors', { tenantId: id, title, content });
+            await request('POST', '/v1/errors', { tenantId: hostId, title, content });
             alert('등록되었습니다.');
+            navigate(-1);
+        } catch (e) {
+            if (isAxiosError(e)) alert(e.response?.data.message);
+        }
+    };
+
+    const patchError = async () => {
+        if (!(title && content)) {
+            alert('내용을 작성해주세요');
+            return;
+        }
+        try {
+            await request('PATCH', `/v1/errors/${id}`, { title, content });
+            alert('수정되었습니다.');
             navigate(-1);
         } catch (e) {
             if (isAxiosError(e)) alert(e.response?.data.message);
@@ -41,7 +72,9 @@ export default function PostQuestion() {
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
             />
-            <button onClick={postError}>등록하기</button>
+            <button onClick={id === '0' ? postError : patchError}>
+                {id === '0' ? '등록하기' : '수정하기'}
+            </button>
         </div>
     );
 }
