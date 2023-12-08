82be3e17dbfdeca5f8901f3b0bb270f973c4f67a
KANG DONGHA <knamuuu@naver.com>
2023-12-08 15:56:02 +0900

    Feat: Sidebar 수정

--- src/components/organisms/Layout/Sidebar/index.tsx
+++ src/components/organisms/Layout/Sidebar/index.tsx
@@ -1,48 +1,72 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import styles from './styles.module.scss';
 
 export default function Sidebar() {
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const [showHowToUse, setShowHowToUse] = useState(false);
+
+    const handleHowToUseClick = () => {
+        setShowHowToUse(!showHowToUse);
+    };
+
+    useEffect(() => {
+        setShowHowToUse(window.location.href.includes('docs'));
+    }, [location]);
 
     return (
         <div className={styles.sidebar}>
             <div className={styles.menuContainer}>
-                <ul
-                    className={styles.menu}
-                    onClick={() => {
-                        navigate('/application');
-                    }}
-                >
-                    <li className="menu-item">
+                <ul>
+                    <li
+                        className={styles.menu}
+                        onClick={() => {
+                            navigate('/application');
+                        }}
+                    >
                         <div className="menu-title">
                             <span>Application</span>
                         </div>
                     </li>
-                </ul>
-                <ul
-                    className={styles.menu}
-                    onClick={() => {
-                        navigate('/question');
-                    }}
-                >
-                    <li className="menu-item">
+                    <li
+                        className={styles.menu}
+                        onClick={() => {
+                            navigate('/question');
+                        }}
+                    >
                         <div className="menu-title">
                             <span>Question</span>
                         </div>
                     </li>
-                </ul>
-                <ul
-                    className={styles.menu}
-                    onClick={() => {
-                        navigate('/billing');
-                    }}
-                >
-                    <li className="menu-item">
-                        <div className="menu-title">
+                    <li
+                        className={styles.menu}
+                        onClick={() => {
+                            navigate('/billing');
+                        }}
+                    >
+                        <div>
                             <span>Billing</span>
                         </div>
+                    </li>
+                    <li className={styles.menu} onClick={handleHowToUseClick}>
+                        <div>
+                            <span>How To Use</span>
+                        </div>
+                        {showHowToUse && (
+                            <ul>
+                                <li onClick={() => navigate('/docs/sdk-docs')}>1. SDK DOCS</li>
+                                <li onClick={() => navigate('/docs/api-docs')}>2. API DOCS</li>
+                                <li onClick={() => navigate('/docs/billing')}>3. 요금 정책</li>
+                                <li onClick={() => navigate('/docs/obs-guide')}>
+                                    4. OBS스튜디오 가이드
+                                </li>
+                                <li onClick={() => navigate('/docs/description')}>5. 용어 정의</li>
+                            </ul>
+                        )}
                     </li>
                 </ul>
             </div>
